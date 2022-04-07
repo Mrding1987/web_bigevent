6c9424e0de3d4134998ee86dd3c191991c877d19
dingj <dingjie54321@163.com>
2022-04-07 22:25:44 +0800

    init project

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('#link_reg').on('click', () => {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    $('#link_login').on('click', () => {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+})
