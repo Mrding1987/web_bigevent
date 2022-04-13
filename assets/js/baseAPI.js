bdff33a1845e04ee34c5a1219a31cf50b70fd83b
dingj <dingjie54321@163.com>
2022-04-13 21:34:12 +0800

    完成了首页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,15 @@
 //options是我们给ajax提供的配置对象
 $.ajaxPrefilter((options) => {
     options.url = 'http://192.168.1.11' + options.url
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+    }
+
+    options.complete = res => {
+
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+    getUserInfo()
+
+    $('#btn_logout').on('click', () => {
+        layui.layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, index => {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layui.layer.close(index)
+        })
+    })
+
+})
+
+function getUserInfo() {
+    $.ajax({
+        type: 'GET',
+        url: '/my/userinfo',
+        success: res => {
+            if (res.status !== 0) return layui.layer.msg('获取用户信息失败')
+            renderAavatar(res.data)
+        },
+
+        //不论ajax的结果是成功还是失败，都会执行这个complete函数,以下函数可以让用户在无权限的i情况下无法访问主页，必须先登录
+        //以下这个complete回调函数可以放在baseAPI.js中，后续不需要每次在这里写，直接从baseAPI中调用
+        /* complete: res => {
+
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+        } */
+
+    })
+}
+
+function renderAavatar(user) {
+    let name = user.nickname || user.username
+
+    // console.log(name);
+    $('.welcome').html('欢迎&nbsp;' + name)
+    if (user.user_pic === null) {
+        $('.layui-nav-img').hide()
+        $('.text_avatar').html(name[0].toUpperCase()).show()
+    } else {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text_avatar').hide()
+    }
+}
